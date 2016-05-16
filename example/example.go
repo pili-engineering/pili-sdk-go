31e80379d0b8da5f6e978956a9e5814df8332fe3
carter2000 <315882215@qq.com>
2016-05-16 15:16:14 +0800

    StreamInfo

--- example/example.go
+++ example/example.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -22,9 +21,108 @@ func init() {
 	SecretKey = os.Getenv("PILI_SECRET_KEY")
 }
 
-func toJSON(v interface{}) string {
-	b, _ := json.Marshal(v)
-	return string(b)
+func createStream(hub *pili.Hub, key string) {
+	stream, err := hub.Create(key)
+	if err != nil {
+		return
+	}
+	info, err := stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println(info)
+}
+
+func getStream(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	info, err := stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println(info)
+}
+
+func listStreams(hub *pili.Hub, prefix string) {
+	keys, marker, err := hub.List(prefix, 10, "")
+	if err != nil {
+		return
+	}
+	fmt.Printf("keys=%v marker=%v\n", keys, marker)
+}
+
+func listLiveStreams(hub *pili.Hub, prefix string) {
+	keys, marker, err := hub.ListLive(prefix, 10, "")
+	if err != nil {
+		return
+	}
+	fmt.Printf("keys=%v marker=%v\n", keys, marker)
+}
+
+func disableStream(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	info, err := stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println("before disable:", info)
+
+	err = stream.Disable()
+	if err != nil {
+		return
+	}
+
+	info, err = stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println("after disable:", info)
+}
+
+func enableStream(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	info, err := stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println("before enable:", info)
+
+	err = stream.Enable()
+	if err != nil {
+		return
+	}
+
+	info, err = stream.Info()
+	if err != nil {
+		return
+	}
+	fmt.Println("after enable:", info)
+}
+
+func liveStatus(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	status, err := stream.LiveStatus()
+	if err != nil {
+		return
+	}
+	fmt.Printf("%+v\n", status)
+}
+
+func historyRecord(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	records, err := stream.HistoryRecord(0, 0)
+	if err != nil {
+		return
+	}
+	fmt.Println(records)
+}
+
+func savePlayback(hub *pili.Hub, key string) {
+	stream := hub.Stream(key)
+	fname, err := stream.Save(0, 0)
+	if err != nil {
+		return
+	}
+	fmt.Println(fname)
 }
 
 func main() {
@@ -38,126 +136,69 @@ func main() {
 	//HubName = "PiliSDKTest"
 	//pili.APIHost = "10.200.20.28:7778"
 
-	// 初始化 client.
+	// 初始化 client & hub.
 	mac := &pili.MAC{AccessKey: AccessKey, SecretKey: []byte(SecretKey)}
 	client := pili.New(mac, nil)
-
-	// 初始化 Hub.
 	hub := client.Hub(HubName)
 
-	// 获得不存在的流.
 	keyA := streamKeyPrefix + "A"
-	_, err := hub.Get(keyA)
-	if !pili.IsNotExists(err) {
-		log.Printf("WARN: keyA=%s should be not exist but got err=(%v)\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 不存在\n", keyA)
+	fmt.Println("获得不存在的流A:")
+	streamA := hub.Stream(keyA)
+	_, err := streamA.Info()
+	fmt.Println(err, "IsNotExists", pili.IsNotExists(err))
 
-	// 创建流.
+	fmt.Println("创建流:")
+	createStream(hub, keyA)
+
+	fmt.Println("获得流:")
+	getStream(hub, keyA)
+
+	fmt.Println("创建重复流:")
 	_, err = hub.Create(keyA)
-	if err != nil {
-		log.Printf("ERROR: keyA=%s create failed err=(%v)\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 创建\n", keyA)
+	fmt.Println(err, "IsExists", pili.IsExists(err))
 
-	// 获得流.
-	streamA, err := hub.Get(keyA)
-	if err != nil {
-		log.Printf("ERROR: keyA=%s get failed err=(%v)\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 查询: %v\n", keyA, toJSON(streamA))
-
-	// 创建重复的流.
-	_, err = hub.Create(keyA)
-	if !pili.IsExists(err) {
-		log.Printf("ERROR: keyA=%s should be exists but got err=(%v)\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 已存在\n", keyA)
-
-	// 创建另一路流.
 	keyB := streamKeyPrefix + "B"
-	streamB, err := hub.Create(keyB)
-	if err != nil {
-		log.Printf("ERROR: keyB=%s create failed %v\n", keyB, err)
-		return
-	}
-	fmt.Printf("keyB=%s 创建: %v\n", keyB, toJSON(streamB))
+	fmt.Println("创建另一路流:")
+	createStream(hub, keyB)
 
-	// 列出所有流.
-	keys, marker, err := hub.List(streamKeyPrefix, 0, "")
-	if err != nil {
-		log.Printf("ERROR: list failed %v\n", err)
-		return
-	}
-	fmt.Printf("hub=%s 列出流: keys=%v marker=%v\n", HubName, keys, marker)
+	fmt.Println("列出流:")
+	listStreams(hub, "carter")
 
-	// 列出正在直播的流.
-	keys, marker, err = hub.ListLive(streamKeyPrefix, 0, "")
-	if err != nil {
-		log.Printf("ERROR: list live failed %v\n", err)
-		return
-	}
-	fmt.Printf("hub=%s 列出正在直播的流: keys=%v marker=%v\n", HubName, keys, marker)
+	fmt.Println("列出正在直播的流:")
+	listLiveStreams(hub, "carter")
 
-	// 禁用流.
-	err = streamA.Disable()
-	if err != nil {
-		log.Printf("ERROR: keyA=%s disable failed err=(%v)\n", keyA, err)
-		return
-	}
-	streamA, err = hub.Get(keyA)
-	if err != nil {
-		log.Printf("ERROR: keyA=%s get failed err=(%v)\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 禁用: %v\n", keyA, toJSON(streamA))
+	fmt.Println("禁用流:")
+	disableStream(hub, keyA)
 
-	// 启用流.
-	err = streamA.Enable()
-	if err != nil {
-		log.Printf("ERROR: keyA=%s enable failed %v\n", keyA, err)
-		return
-	}
-	streamA, err = hub.Get(keyA)
-	if err != nil {
-		log.Printf("ERROR: keyA=%s get failed %v\n", keyA, err)
-		return
-	}
-	fmt.Printf("keyA=%s 启用: %v\n", keyA, toJSON(streamA))
+	fmt.Println("启用流:")
+	enableStream(hub, keyA)
 
-	// 查询直播状态.
-	status, err := streamA.LiveStatus()
-	fmt.Printf("keyA=%s 直播状态: status=%v err=(%v)\n", keyA, toJSON(status), err)
+	fmt.Println("查询直播状态:")
+	liveStatus(hub, keyA)
 
-	// 查询推流历史.
-	records, err := streamA.HistoryRecord(0, 0)
-	fmt.Printf("keyA=%s 推流历史: records=%v err=(%v)\n", keyA, records, err)
+	fmt.Println("查询推流历史:")
+	historyRecord(hub, keyA)
 
-	// 保存直播数据.
-	fname, err := streamA.Save(0, 0)
-	fmt.Printf("keyA=%s 保存直播数据: fname=%s err=(%v)\n", keyA, fname, err)
+	fmt.Println("保存直播数据:")
+	savePlayback(hub, keyA)
 
-	// RTMP 推流地址.
+	fmt.Println("RTMP 推流地址:")
 	url := pili.RTMPPublishURL("publish-rtmp.test.com", HubName, keyA, mac, 3600)
-	fmt.Printf("keyA=%s RTMP推流地址=%s\n", keyA, url)
+	fmt.Println(url)
 
-	// RTMP 直播放址.
+	fmt.Println("RTMP 直播放址:")
 	url = pili.RTMPPlayURL("live-rtmp.test.com", HubName, keyA)
-	fmt.Printf("keyA=%s RTMP直播地址=%s\n", keyA, url)
+	fmt.Println(url)
 
-	// HLS 直播地址.
+	fmt.Println("HLS 直播地址:")
 	url = pili.HLSPlayURL("live-hls.test.com", HubName, keyA)
-	fmt.Printf("keyA=%s HLS直播地址=%s\n", keyA, url)
+	fmt.Println(url)
 
-	// HDL 直播地址.
+	fmt.Println("HDL 直播地址:")
 	url = pili.HDLPlayURL("live-hdl.test.com", HubName, keyA)
-	fmt.Printf("keyA=%s HDL直播地址=%s\n", keyA, url)
+	fmt.Println(url)
 
-	// 截图直播地址
+	fmt.Println("截图直播地址:")
 	url = pili.SnapshotPlayURL("live-snapshot.test.com", HubName, keyA)
-	fmt.Printf("keyA=%s 截图直播地址=%s\n", keyA, url)
+	fmt.Println(url)
 }
--- pili/stream_test.go
+++ pili/stream_test.go
@@ -2,9 +2,29 @@ package pili
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestStreamInfo(t *testing.T) {
+
+	info := StreamInfo{"a", "b", 0}
+	require.False(t, info.Disabled())
+	require.Equal(t, info.String(), "{hub:a,key:b,disabled:false}")
+
+	info = StreamInfo{"a", "b", -1}
+	require.True(t, info.Disabled())
+	require.Equal(t, info.String(), "{hub:a,key:b,disabled:true}")
+
+	info = StreamInfo{"a", "b", 12345}
+	require.False(t, info.Disabled())
+	require.Equal(t, info.String(), "{hub:a,key:b,disabled:false}")
+
+	info = StreamInfo{"a", "b", time.Now().Unix() + 1000}
+	require.True(t, info.Disabled())
+	require.Equal(t, info.String(), "{hub:a,key:b,disabled:true}")
+}
 
 func TestStream(t *testing.T) {
 	if skipTest() {
@@ -18,23 +38,26 @@ func TestStream(t *testing.T) {
 	key := testStreamPrefix + "TestStream"
 	_, err := hub.Create(key)
 	require.NoError(t, err)
-	stream, err := hub.Get(key)
+	stream := hub.Stream(key)
 	require.NoError(t, err)
-	require.True(t, checkStream(stream, testHub, key, false))
+	info, err := stream.Info()
+	require.NoError(t, err)
+	require.True(t, checkStream(info, testHub, key, false))
 
 	// Disable.
+	stream = hub.Stream(key)
 	err = stream.Disable()
 	require.NoError(t, err)
-	stream, err = hub.Get(key)
+	info, err = stream.Info()
 	require.NoError(t, err)
-	require.True(t, checkStream(stream, testHub, key, true))
+	require.True(t, checkStream(info, testHub, key, true))
 
 	// Enable.
 	err = stream.Enable()
 	require.NoError(t, err)
-	stream, err = hub.Get(key)
+	info, err = stream.Info()
 	require.NoError(t, err)
-	require.True(t, checkStream(stream, testHub, key, false))
+	require.True(t, checkStream(info, testHub, key, false))
 
 	// LiveStatus, no live.
 	_, err = stream.LiveStatus()
